d4fbda41080a7c6cdc450190fc82e36a587e00f4
Com1 Software, Inc <information@com1software.com>
2023-09-13 18:20:48 -0400

    Update main.ino

--- INO/Example3/main.ino
+++ INO/Example3/main.ino
@@ -10,21 +10,9 @@ void setup() {
   pinMode(RC_CH1_INPUT, INPUT);
   attachInterrupt(digitalPinToInterrupt(RC_CH1_INPUT), READ_RC1, CHANGE);
 }
-
-// Thee functions are called by the interrupts. We send them all to the same place to measure the pulse width
 void READ_RC1() { 
    Read_Input(RC_CH1, RC_CH1_INPUT); 
 }
-void READ_RC2() { 
-   Read_Input(RC_CH2, RC_CH2_INPUT);
-}
-void READ_RC3() { 
-   Read_Input(RC_CH3, RC_CH3_INPUT); 
-}
-void READ_RC4() { 
-   Read_Input(RC_CH4, RC_CH4_INPUT); 
-}
-// This function reads the pulse starts and uses the time between rise and fall to set the value for pulse width
 void Read_Input(uint8_t channel, uint8_t input_pin) {
   if (digitalRead(input_pin) == HIGH) {
     RC_START[channel] = micros();
@@ -35,17 +23,11 @@ void Read_Input(uint8_t channel, uint8_t input_pin) {
 }
 
 void loop() {
-  
-  // read the values from our RC Receiver
   rc_read_values();
 Serial.print("CH1=");
-Serial.print(RC_VALUES[RC_CH1]); Serial.print(",CH2=");
-Serial.print(RC_VALUES[RC_CH2]); Serial.print(",CH3=");
-Serial.print(RC_VALUES[RC_CH3]); Serial.print(",CH4=");
-Serial.print(RC_VALUES[RC_CH4]);Serial.print("\n");
+Serial.print(RC_VALUES[RC_CH1]); 
+Serial.print("\n");
 }
-
-// this function pulls the current values from our pulse arrays for us to use. 
 void rc_read_values() {
   noInterrupts();
   memcpy(RC_VALUES, (const void *)RC_SHARED, sizeof(RC_SHARED));
