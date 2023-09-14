b64f62910dc30637faf0e377da524a621b622d7c
Com1 Software, Inc <information@com1software.com>
2023-09-14 17:26:33 -0400

    Update main.ino

--- INO/Example3/main.ino
+++ INO/Example3/main.ino
@@ -3,12 +3,25 @@ void setup() {
   Serial.begin(115200);
   pinMode(2,INPUT);
   pinMode(3,OUTPUT);
+  pinMode(4,INPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,INPUT);
+  pinMode(7,OUTPUT);
 
 }
 
 void loop() {
   analogWrite(3,map(pulseIn(2,HIGH),1100,1900,0,255));
-  Serial.println(map(pulseIn(2,HIGH),1100,1900,0,255));
+  Serial.print("CH1=");
+  Serial.print(map(pulseIn(2,HIGH),1100,1900,0,255));
+  analogWrite(5,map(pulseIn(4,HIGH),1100,1900,0,255));
+  Serial.print(",CH2=");
+  Serial.print(map(pulseIn(4,HIGH),1100,1900,0,255));
+  analogWrite(7,map(pulseIn(6,HIGH),1100,1900,0,255));
+  Serial.print(",CH3=");
+  Serial.print(map(pulseIn(6,HIGH),1100,1900,0,255));
+  Serial.print(",CH4=");
+  ;Serial.print("\n");
   delay(20);
 
 }
